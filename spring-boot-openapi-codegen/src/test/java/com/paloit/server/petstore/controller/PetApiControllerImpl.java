9bcacaa35d63de01702bc85ed544148562936548
Peter Weismann <pweismann@th.palo-it.com>
2022-01-25 19:22:29 +0700

    Rename test package

--- spring-boot-openapi-codegen/src/test/java/com/paloit/server/petstore/controller/PetApiControllerImpl.java
+++ spring-boot-openapi-codegen/src/test/java/com/paloit/server/petstore/controller/PetApiControllerImpl.java
@@ -1,6 +1,5 @@
-package com.paloit.server.petstore.service;
+package com.paloit.server.petstore.controller;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- spring-boot-openapi-codegen/src/test/java/com/paloit/server/petstore/controller/StoreApiControllerImpl.java
+++ spring-boot-openapi-codegen/src/test/java/com/paloit/server/petstore/controller/StoreApiControllerImpl.java
@@ -1,11 +1,10 @@
-package com.paloit.server.petstore.service;
+package com.paloit.server.petstore.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
